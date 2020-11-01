bf8e31a2e73109349ed81a26a602b76c592bdfa9
unknown <carterjamahl@gmail.com>
2020-11-01 17:36:31 +1100

    Added terraform template for configuring ASB resource

---
+++ sync-adapter/ioc/main.tf
@@ -0,0 +1,40 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 2.26"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-sync-adapter-example"
+  location = "australiaeast"
+}
+
+resource "azurerm_servicebus_namespace" "ns" {
+  name                = "ns-sync-adapter-servicebus"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "Standard"
+}
+
+resource "azurerm_servicebus_queue" "request" {
+  name                = "RequestQueue"
+  resource_group_name = azurerm_resource_group.rg.name
+  namespace_name      = azurerm_servicebus_namespace.ns.name
+  max_delivery_count  = 10
+}
+
+resource "azurerm_servicebus_queue" "reply" {
+  name                = "ReplyQueue"
+  resource_group_name = azurerm_resource_group.rg.name
+  namespace_name      = azurerm_servicebus_namespace.ns.name
+  requires_session    = true
+  max_delivery_count  = 10
+}
